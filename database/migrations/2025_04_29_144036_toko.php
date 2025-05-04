3d53dcce5ba333ff92937573233d726bb47cddac
SuardiTI <Suardii2005@gmail.com>
2025-05-05 00:26:39 +0800

    membuat tabel di migration

--- database/migrations/2025_04_29_144036_toko.php
+++ database/migrations/2025_04_29_144036_toko.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Symfony\Component\Mailer\Transport\Dsn;
 
 return new class extends Migration
 {
@@ -38,6 +39,7 @@ return new class extends Migration
             $table->string('path_gambar');
             $table->timestamps();
         });
+        
         
 
         Schema::create('orders', function (Blueprint $table) {
