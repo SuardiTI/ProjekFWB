7b0438c858ee868f98c646da3cca8273b1ee4f1a
SuardiTI <Suardii2005@gmail.com>
2025-05-19 01:37:22 +0800

    integrasi template untuk form login dan register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -25,20 +25,6 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div><br>
 
-            {{-- role --}}
-            {{-- <div>
-                <x-input-label for="role" :value="__('Role')" />
-            
-                <select id="role" name="role" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" required>
-                    <option value="" disabled selected>-- Pilih Role --</option>
-                    <option value="admin" @selected(old('role') == 'admin')>Admin</option>
-                    <option value="pembeli" @selected(old('role') == 'pembeli')>Pembeli</option>
-                    <option value="penjual" @selected(old('role') == 'penjual')>Penjual</option>
-                </select>
-            
-                <x-input-error :messages="$errors->get('role')" class="mt-2" />
-            </div> --}}
-
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
@@ -59,4 +45,194 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+
+
+
+<!--
+=========================================================
+* Material Dashboard 3 - v3.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://www.creative-tim.com/license)
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  {{-- <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('material') }}/assets/img/apple-icon.png"> --}}
+  {{-- <link rel="icon" type="image/png" href="{{ asset('material') }}/assets/img/favicon.png"> --}}
+  <title>
+    Material Dashboard 3 by Creative Tim
+  </title>
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />
+  <!-- Nucleo Icons -->
+  <link href="{{ asset('material') }}/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="{{ asset('material') }}/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <!-- Material Icons -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="{{ asset('material') }}/assets/css/material-dashboard.css?v=3.2.0" rel="stylesheet" />
+</head>
+
+<body class="bg-gray-200">
+  <div class="container position-sticky z-index-sticky top-0">
+    <div class="row">
+      <div class="col-12">
+      </div>
+    </div>
+  </div>
+  <main class="main-content  mt-0">
+    {{-- <div class="page-header align-items-start min-vh-100" style="background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');"> --}}
+    <div class="page-header align-items-start min-vh-100" style="background-image: url('{{ asset('ikonpubg2.jpg')}}')">
+
+      <span class="mask bg-gradient-dark opacity-6"></span>
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-4 col-md-8 col-12 mx-auto">
+            <div class="card z-index-0 fadeIn3 fadeInBottom">
+              <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                <div class="bg-gradient-dark shadow-dark border-radius-lg py-3 pe-1">
+                  <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Sign in</h4>
+                  <div class="row mt-3">
+                    <div class="col-2 text-center ms-auto">
+                      <a class="btn btn-link px-3" href="javascript:;">
+                        <i class="fa fa-facebook text-white text-lg"></i>
+                      </a>
+                    </div>
+                    <div class="col-2 text-center px-1">
+                      <a class="btn btn-link px-3" href="javascript:;">
+                        <i class="fa fa-github text-white text-lg"></i>
+                      </a>
+                    </div>
+                    <div class="col-2 text-center me-auto">
+                      <a class="btn btn-link px-3" href="javascript:;">
+                        <i class="fa fa-google text-white text-lg"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card-body">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+               
+                     <x-auth-session-status class="mb-4 text-danger" :status="session('status')" />
+
+                     <div class="input-group input-group-outline my-3">
+                         <x-input-label for="email" class="form-label" :value="__('Email')" />
+                         <input id="email" type="email" name="email" class="form-control" :value="old('email')" required autofocus autocomplete="username">
+                     </div>
+                     <x-input-error :messages="$errors->get('email')" class="text-danger text-sm mb-2" />
+
+                     <div class="input-group input-group-outline mb-3">
+                         <x-input-label for="password" class="form-label" :value="__('Password')" />
+                         <input id="password" type="password" name="password" class="form-control" required autocomplete="current-password">
+                     </div>
+                     <x-input-error :messages="$errors->get('password')" class="text-danger text-sm mb-2" />
+
+                     <div class="form-check form-switch d-flex align-items-center mb-3">
+                         <input class="form-check-input" type="checkbox" name="remember" id="remember_me">
+                         <label class="form-check-label mb-0 ms-3" for="remember_me">
+                             {{ __('Remember me') }}
+                         </label>
+                     </div>
+                 
+                  
+                     <div class="text-center">
+                         <button type="submit" class="btn bg-gradient-dark w-100 my-4 mb-2">
+                             {{ __('Log in') }}
+                         </button>
+                     </div>
+                 
+         
+                     @if (Route::has('password.request'))
+                         <p class="text-center">
+                             <a class="text-sm text-primary text-gradient font-weight-bold" href="{{ route('password.request') }}">
+                                 {{ __('Forgot your password?') }}
+                             </a>
+                         </p>
+                     @endif
+                     
+            
+                     <p class="mt-4 text-sm text-center">
+                         {{ __("Don't have an account?") }}
+                         <a href="{{ route('register') }}" class="text-primary text-gradient font-weight-bold">
+                             {{ __('Sign up') }}
+                         </a>
+                     </p>
+                 </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer class="footer position-absolute bottom-2 py-2 w-100">
+        <div class="container">
+          <div class="row align-items-center justify-content-lg-between">
+            <div class="col-12 col-md-6 my-auto">
+              <div class="copyright text-center text-sm text-white text-lg-start">
+                © <script>
+                  document.write(new Date().getFullYear())
+                </script>,
+                made with <i class="fa fa-heart" aria-hidden="true"></i> by
+                <a href="https://www.creative-tim.com" class="font-weight-bold text-white" target="_blank">Creative Tim</a>
+                for a better web.
+              </div>
+            </div>
+            <div class="col-12 col-md-6">
+              <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                <li class="nav-item">
+                  <a href="https://www.creative-tim.com" class="nav-link text-white" target="_blank">Creative Tim</a>
+                </li>
+                <li class="nav-item">
+                  <a href="https://www.creative-tim.com/presentation" class="nav-link text-white" target="_blank">About Us</a>
+                </li>
+                <li class="nav-item">
+                  <a href="https://www.creative-tim.com/blog" class="nav-link text-white" target="_blank">Blog</a>
+                </li>
+                <li class="nav-item">
+                  <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-white" target="_blank">License</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </main>
+  <!--   Core JS Files   -->
+  <script src="{{ asset('material') }}/assets/js/core/popper.min.js"></script>
+  <script src="{{ asset('material') }}/assets/js/core/bootstrap.min.js"></script>
+  <script src="{{ asset('material') }}/assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="{{ asset('material') }}/assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="{{ asset('material') }}/assets/js/material-dashboard.min.js?v=3.2.0"></script>
+</body>
+
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+ {{-- <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
     
@@ -38,7 +38,7 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div><br>
 
-            {{-- role --}}
+            {{-- role -
             <div>
                 <x-input-label for="role" :value="__('Role')" />
             
@@ -61,5 +61,164 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> 
+ --}}
 
+
+
+
+<!--
+=========================================================
+* Material Dashboard 3 - v3.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://www.creative-tim.com/license)
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('material') }}/assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="{{ asset('material') }}/assets/img/favicon.png">
+  <title>
+    Material Dashboard 3 by Creative Tim
+  </title>
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />
+  <!-- Nucleo Icons -->
+  <link href="{{ asset('material') }}/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="{{ asset('material') }}/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <!-- Material Icons -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="{{ asset('material') }}/assets/css/material-dashboard.css?v=3.2.0" rel="stylesheet" />
+</head>
+
+<body class="">
+  <div class="container position-sticky z-index-sticky top-0">
+    <div class="row">
+      <div class="col-12">
+      </div>
+    </div>
+  </div>
+  <main class="main-content  mt-0">
+    <section>
+      <div class="page-header min-vh-100">
+        <div class="container">
+          <div class="row">
+            <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 start-0 text-center justify-content-center flex-column">
+              {{-- <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center" style="background-image: url('{{ asset('material') }}/assets/img/illustrations/illustration-signup.jpg'); background-size: cover;"> --}}
+              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center" style="background-image: url('{{ asset('backroundlogin.jpg') }}'); background-size: cover;">
+              </div>
+            </div>
+            <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column ms-auto me-auto ms-lg-auto me-lg-5">
+              <div class="card card-plain">
+                <div class="card-header">
+                  <h4 class="font-weight-bolder">Sign Up</h4>
+                  <p class="mb-0">Enter your email and password to register</p>
+                </div>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+                    
+                        <!-- Name -->
+                        <div class="input-group input-group-outline mb-3">
+                            <label class="form-label" for="name">Name</label>
+                            <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus autocomplete="name">
+                            @error('name')
+                                <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                    
+                        <!-- Email -->
+                        <div class="input-group input-group-outline mb-3">
+                            <label class="form-label" for="email">Email</label>
+                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="username">
+                            @error('email')
+                                <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                    
+                        <!-- Password -->
+                        <div class="input-group input-group-outline mb-3">
+                            <label class="form-label" for="password">Password</label>
+                            <input id="password" type="password" class="form-control" name="password" required autocomplete="new-password">
+                            @error('password')
+                                <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                    
+                        <!-- Confirm Password -->
+                        <div class="input-group input-group-outline mb-3">
+                            <label class="form-label" for="password_confirmation">Confirm Password</label>
+                            <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            @error('password_confirmation')
+                                <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                    
+                        <!-- Role -->
+                        <div class="input-group input-group-outline mb-3">
+                            <label class="form-label" for="role" :value="__('Role')"></label></label>
+                            <select id="role" name="role" class="form-control">
+                                <option value="" disabled selected>-- Pilih Role --</option>
+                                <option value="pembeli" @selected(old('role') == 'pembeli')>Pembeli</option>
+                                <option value="penjual" @selected(old('role') == 'penjual')>Penjual</option>
+                            </select>
+                            @error('role')
+                                <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                    
+                        <!-- Submit Button -->
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-lg bg-gradient-dark w-100 mt-4 mb-0">Register</button>
+                        </div>
+                    </form>
+                    
+                </div>
+                <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                  <p class="mb-2 text-sm mx-auto">
+                    Already have an account?
+                    <a href="{{ route('login') }}" class="text-primary text-gradient font-weight-bold">Login</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <!--   Core JS Files   -->
+  <script src="{{ asset('material') }}/assets/js/core/popper.min.js"></script>
+  <script src="{{ asset('material') }}/assets/js/core/bootstrap.min.js"></script>
+  <script src="{{ asset('material') }}/assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="{{ asset('material') }}/assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="{{ asset('material') }}/assets/js/material-dashboard.min.js?v=3.2.0"></script>
+</body>
+
+</html>
